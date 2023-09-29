c250182f665297db8e6797662fbe896848f305fa
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-09-28 21:25:09 -0700

    Write evaluate function

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ def display_accuracy(target, predictions, labels, title):
     ax.set_title(title)
     plt.show()
 
+def evaluate(model, X_train, X_test, y_train, y_test):
+    model.fit(X_train, y_train)
+
+    y_pred_train = model.predict(X_train)
+    print(f'{model.__class__.__name__} Train Accuracy: {(y_pred_train == y_train).mean()}')
+    display_accuracy(y_train, y_pred_train, ["Rock", "Mine"], "Confusion matrix")
+
+    y_pred_test = model.predict(X_test)
+    print(f'{model.__class__.__name__} Test Accuracy: {(y_pred_test == y_test).mean()}')
+    display_accuracy(y_test, y_pred_test, ["Rock", "Mine"], "Confusion matrix")
 
 def main():
     # setting up train and test set
@@ -20,26 +30,13 @@ def main():
     target = np.genfromtxt('sonar.all-data.csv', usecols=[60], dtype=np.dtype('U1'), delimiter=',') == 'M'
     X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.2, random_state=42)
 
-    # Nearal Network Model
-    #classifier = MLPClassifier(random_state=42, hidden_layer_sizes=30, batch_size= 10, max_iter= 500)
-    #classifier.fit(X_train, y_train)
+    evaluate(MLPClassifier(
+        random_state=0, hidden_layer_sizes=30, batch_size=10, max_iter=500, verbose=1
+    ), X_train, X_test, y_train, y_test)
 
-    # random forest classifier
-    classifier = RandomForestClassifier(random_state=42, n_estimators= 500)
-    classifier.fit(X_train, y_train)
-
-
-    # predictions
-    y_pred_train = classifier.predict(X_train)
-    print(f'Train Accuracy: {(y_pred_train == y_train).mean()}')
-    display_accuracy(y_train, y_pred_train, ["Rock", "Mine"], "Confusion matrix")
-
-    y_pred_test = classifier.predict(X_test)
-    print(f'Test Accuracy: {(y_pred_test == y_test).mean()}')
-    display_accuracy(y_test, y_pred_test, ["Rock", "Mine"], "Confusion matrix")
-
+    evaluate(RandomForestClassifier(
+        random_state=0, n_estimators=500,
+    ), X_train, X_test, y_train, y_test)
 
 if __name__ == '__main__':
     main()
-
-
